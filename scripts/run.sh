e4b285329ac6422cfca3bd9566fb34bd007914ef
tss52 <tss52@cam.ac.uk>
2025-05-18 17:45:22 +0100

    Add run script

---
+++ scripts/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+uv run accelerate launch --num-processes 4 --num_machines 1 --deepspeed_config_file /home/tss52/rds/hpc-work/RLMEval/src/deepspeed.conf --machine_rank $SLURM_PROCID /home/tss52/rds/hpc-work/RLMEval/src/train.py --server_ip $VLLM_NODE &
+uv run uvicorn src.lean_pool_api:app --port 8080
