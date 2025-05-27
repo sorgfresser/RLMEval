bd99fc4767cea2ddae5a77fe46f09ed6fb9c8083
Theo Simon Sorg <tss52@cam.ac.uk>
2025-05-27 19:30:44 +0000

    Split run into two

--- scripts/run_vllm.sh
+++ scripts/run_vllm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
-uv run trl vllm-serve --model AI-MO/Kimina-Autoformalizer-7B --tensor_parallel_size 1 &
+uv run trl vllm-serve --model sorgfresser/testtrainsft --tensor_parallel_size 1 &
