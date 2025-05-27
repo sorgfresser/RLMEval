a9d81c21932844eb67ced7af01041bdd39f3035f
Theo Simon Sorg <tss52@cam.ac.uk>
2025-05-27 19:29:43 +0000

    Split run into two

--- scripts/run_trainer.sh
+++ scripts/run_trainer.sh
@@ -3,6 +3,6 @@ uv run uvicorn src.lean_pool_api:app --port 8020 &
 
 sleep 180
 
-export CUDA_VISIBLE_DEVICES=1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2
 uv run accelerate launch --num-processes 3 --num_machines 1 --deepspeed_config_file /home/tss52/rds/hpc-work/RLMEval/src/deepspeed.conf --machine_rank $SLURM_PROCID /home/tss52/rds/hpc-work/RLMEval/src/train.py --server-ip localhost &
 wait
