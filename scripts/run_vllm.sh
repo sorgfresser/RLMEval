94fdc4b51bfcf482ad5fde78af19856fcb58691e
Theo Simon Sorg <tss52@cam.ac.uk>
2025-05-27 21:06:00 +0000

    Downgr

--- scripts/run_vllm.sh
+++ scripts/run_vllm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
-uv run trl vllm-serve --model sorgfresser/testtrainsft --tensor_parallel_size 1 &
+uv run ./scripts/vllm_serve.py --model sorgfresser/testtrainsft --tensor_parallel_size 1
