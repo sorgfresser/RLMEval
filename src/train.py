7520d06acf8cae5c8dd452167150b9b93bc3d02e
tss52 <tss52@cam.ac.uk>
2025-05-30 14:06:06 +0100

    Add embedding reward

--- src/train.py
+++ src/train.py
@@ -12,14 +12,42 @@ import concurrent.futures
 import logging
 import os
 from transformers import AutoTokenizer
+from FlagEmbedding import BGEM3FlagModel
+import torch
 
 logger = logging.getLogger(__name__)
 
-MAX_WORKERS = int(os.getenv("LEAN_POOL_MAX_WORKERS", 26))
-MODEL_NAME="sorgfresser/testtrainsft"
+MAX_WORKERS = int(os.getenv("LEAN_POOL_MAX_WORKERS", 1))
+model_path = "purewhite42/dependency_retriever_f"
+model = BGEM3FlagModel(model_path, use_fp16=True, devices=["cpu"])
+batch_size=64
+
+def format_doc_only_f(decl: str) -> str:
+    return f'''Formal Declaration: {decl[:1536]}'''
 
 
-def get_beqplus(context: str, ground_truth: str, prediction: str, project: str, is_theorem: bool) -> float:
+def get_embedding_sim(completions: list[list[dict[str, str]]], informal: list[str], **kwargs) -> list[float]:
+    completions = [completion[0]["content"] for completion in completions]
+    formal_statements = [format_doc_only_f(pred) for pred in completions]
+    informal_statements = informal
+
+    formal_embeddings = model.encode(
+        formal_statements,
+        batch_size=batch_size,
+        max_length=1024)['dense_vecs']
+    informal_embeddings = model.encode(
+        informal_statements,
+        batch_size=batch_size,
+        max_length=1024
+    )['dense_vecs']
+
+    similarity = (torch.tensor(informal_embeddings).double() @ torch.tensor(formal_embeddings).double().T)
+
+    diagonals = similarity.diagonal()
+    return diagonals.tolist()
+
+
+def get_beqplus(context: str, ground_truth: str, prediction: str, project: str, is_theorem: bool, beqplus: bool = True) -> float:
     # check if the last line ends with " in" and remove " in" if it does
     trimmed_context = trim_comments_end(context).rstrip()
     if "\n" in trimmed_context:
@@ -55,7 +83,7 @@ def get_beqplus(context: str, ground_truth: str, prediction: str, project: str, 
         else:
             well_typed = prediction_data.lean_code_is_valid(allow_sorry=is_theorem)
             beq_result = None
-            if well_typed and is_theorem:
+            if well_typed and is_theorem and beqplus:
                 beq_result = check_theorem_eq_server(
                     theorem1=prediction,
                     theorem2=ground_truth,
@@ -75,7 +103,7 @@ def get_beqplus(context: str, ground_truth: str, prediction: str, project: str, 
 
 def make_conversational(batch):
     all_messages = []
-    for name, problem, context in zip(batch["name"], batch["prompt"], batch["context"], strict=True):
+    for name, problem, context in zip(batch["name"], batch["informal"], batch["context"], strict=True):
         prompt = f"Please autoformalize the following problem in Lean 4 with a header. Here is some context for the problem:\n\n{context}\n\nNow formalize the problem. Use the following theorem names: {name}.\n\n"
         prompt += problem
 
@@ -96,11 +124,17 @@ def split_func(sample, train: bool = True):
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("model_name", type=str)
+    parser.add_argument("output_dir", type=str)
     parser.add_argument("--server-ip", type=str, default="localhost")
+    parser.add_argument("--beta", type=float, default=0.0)
+    parser.add_argument("--embedding-sim", action="store_true", default=False)
+    parser.add_argument("--no-beq", action="store_true", default=False)
+    parser.add_argument("--resume", action="store_true", default=False)
     args = parser.parse_args()
 
     dataset = load_dataset("sorgfresser/autoformtest", split="train")
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
 
     def filter_length(elem, length: int = tokenizer.model_max_length):
         if len(tokenizer(tokenizer.apply_chat_template(elem["prompt"], tokenize=False, add_generation_prompt=True)).input_ids) > length:
@@ -108,7 +142,6 @@ def main():
         return True
 
     # Rename informal to prompt, at some point one might want to add context
-    dataset = dataset.rename_columns({"informal": "prompt"})
     dataset = dataset.map(make_conversational, batch_size=1000, batched=True)
     dataset = dataset.filter(lambda x : filter_length(x, length=tokenizer.model_max_length - 1024))
     train_dataset = dataset.filter(split_func)
@@ -121,8 +154,9 @@ def main():
         rewards = [r if c and c[0]["role"] == "assistant" else 0.0 for r, c in zip(rewards, completions, strict=True)]
         completion_strings = [None if r is not None else c[0]["content"] for r, c in
                               zip(rewards, completions, strict=True)]
+        beqplus = not args.no_beq
         with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as pool:
-            futures = {pool.submit(get_beqplus, con, g, c, p, t): idx for idx, (con, g, c, p, t) in
+            futures = {pool.submit(get_beqplus, con, g, c, p, t, beqplus): idx for idx, (con, g, c, p, t) in
                        enumerate(zip(context, ground_truth, completion_strings, project, is_theorem, strict=True))}
             failures = 0
             for fut in concurrent.futures.as_completed(futures):
@@ -144,7 +178,7 @@ def main():
         return rewards
 
     training_args = GRPOConfig(
-        output_dir="Kimina-GRPO",
+        output_dir=args.output_dir,
         per_device_train_batch_size=8,
         gradient_accumulation_steps=16,
         bf16=True,
@@ -156,7 +190,7 @@ def main():
         warmup_steps=5,
         report_to=["wandb"],
         max_completion_length=1024,
-        beta=0,
+        beta=args.beta,
         vllm_server_host=args.server_ip,
         loss_type="dr_grpo",
         learning_rate=1e-6,
@@ -168,11 +202,14 @@ def main():
         push_to_hub=True,
         lr_scheduler_type="constant_with_warmup"
     )
+    reward_funcs = [reward_num_unique_chars]
+    if args.embedding_sim:
+        reward_funcs.append(get_embedding_sim)
 
-    trainer = GRPOTrainer(model="sorgfresser/testtrainsft", args=training_args,
-                          reward_funcs=[reward_num_unique_chars], train_dataset=train_dataset,
+    trainer = GRPOTrainer(model=args.model_name, args=training_args,
+                          reward_funcs=reward_funcs, train_dataset=train_dataset,
                           eval_dataset=test_dataset)
-    trainer.train()
+    trainer.train(resume_from_checkpoint=args.resume)
 
 
 if __name__ == "__main__":
