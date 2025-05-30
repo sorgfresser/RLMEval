86cb4aeb2d85ea259b9d2b6355a7a206ba33d497
Simon Sorg <tss52@cam.ac.uk>
2025-05-30 01:45:06 +0100

    Remove cuda visible devices

--- scripts/run_trainer.sh
+++ scripts/run_trainer.sh
@@ -3,6 +3,5 @@ uv run uvicorn src.lean_pool_api:app --port 8020 &
 
 sleep 180
 
-export CUDA_VISIBLE_DEVICES=0,1,2
 uv run accelerate launch --num-processes 3 --num_machines 1 --deepspeed_config_file /home/tss52/rds/hpc-work/RLMtrain/src/deepspeedconf.json --machine_rank $SLURM_PROCID /home/tss52/rds/hpc-work/RLMtrain/src/train.py --server-ip localhost &
 wait
--- scripts/run_vllm.sh
+++ scripts/run_vllm.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
 uv run ./scripts/vllm_serve.py --model sorgfresser/testtrainsft --tensor_parallel_size 1
