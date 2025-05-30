c04823d324a244de454ae6c4dfe00861347fbd09
tss52 <tss52@cam.ac.uk>
2025-05-30 14:11:49 +0100

    Remove separate run vllm, update trainer to handle args

--- scripts/run_trainer.sh
+++ scripts/run_trainer.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+ulimit -n 8192
 uv run uvicorn src.lean_pool_api:app --port 8020 &
+
+export CUDA_VISIBLE_DEVICES=0
+uv run ./scripts/vllm_serve.py --model sorgfresser/testtrainsft --tensor_parallel_size 1 &
 
 sleep 180
 
-uv run accelerate launch --num-processes 3 --num_machines 1 --deepspeed_config_file /home/tss52/rds/hpc-work/RLMtrain/src/deepspeedconf.json --machine_rank $SLURM_PROCID /home/tss52/rds/hpc-work/RLMtrain/src/train.py --server-ip localhost &
+export CUDA_VISIBLE_DEVICES=1,2,3
+uv run accelerate launch --num-processes 3 --num_machines 1 --deepspeed_config_file /home/tss52/rds/hpc-work/RLMtrain/src/deepspeedconf.json --machine_rank "${SLURM_PROCID:-0}" /home/tss52/rds/hpc-work/RLMtrain/src/train.py --server-ip localhost "$@" &
 wait
--- scripts/run_vllm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uv run ./scripts/vllm_serve.py --model sorgfresser/testtrainsft --tensor_parallel_size 1
